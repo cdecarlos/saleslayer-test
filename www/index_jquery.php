7f51b91637d4addaa602bfa0dd905b8d1f40c46f
Carlos M <carlos@cdecarlos.com>
2022-02-23 13:10:20 +0100

    add index and index jquery

---
+++ www/index_jquery.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="css/app.min.css" rel="stylesheet">
+
+    <title>Info countries</title>
+  </head>
+  <body>
+    <?php include_once 'views/_header.php'; ?>
+
+    <?php include_once 'views/_main.php'; ?>
+
+    <?php include_once 'views/_footer.php'; ?>
+
+    <div class="container">
+      <a href="index.php">Ir a versión js vanilla</a>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/numeral.js/2.0.6/numeral.min.js"></script>
+
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script src="js/class/Api.js"></script>
+    <script src="js/class/CountryJSON.js"></script>
+    <script src="js/class/LocalStorage.js"></script>
+
+    <script src="js/lib/common.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="js/lib/libJquery.js"></script>
+
+    <script src="js/app.js"></script>
+  </body>
+</html>
